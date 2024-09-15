c479f1fee857fc6da9ea680446dcb75f72b9b965
Karan Aulakh <karan.aulakh@workday.com>
2024-09-14 19:47:00 -0600

    update folder to save to

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args){
         Downloader downloader = new Downloader();
         try {
-            downloader.downLoad("https://www.journaldev.com/sitemap.xml", "/Users/karan.aulakh/Desktop/Karan/Downloader/downloaded_files");
+            downloader.downLoad("https://www.journaldev.com/sitemap.xml", "/Users/karan.aulakh/Desktop/Karan/Downloader/downloaded_files/test");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
