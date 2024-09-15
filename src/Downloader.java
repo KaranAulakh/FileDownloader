c1f150c5e6e248351b1e211d8f1e22442711e5fb
Karan Aulakh <karan.aulakh@workday.com>
2024-09-14 19:33:42 -0600

    Basic Downloader

--- src/Downloader.java
+++ src/Downloader.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class Downloader {
+    // will use later for threading
+    private boolean isDownloading;
+
+    public Downloader(){
+        this.isDownloading = false;
+    }
+
+
+    public void downLoad (String urlToDownload, String downloadPath) throws IOException {
+        isDownloading = true;
+        URL url = new URL(urlToDownload);
+        try (InputStream inputStream = url.openStream()) {
+            FileOutputStream fileOutputStream = new FileOutputStream(downloadPath);
+            byte[] bytes = new byte[1024];
+            int len;
+            while ((len = inputStream.read(bytes)) != 0){
+                fileOutputStream.write(bytes, 0, len);
+            }
+            fileOutputStream.close();
+            System.out.println("Downloaded: " + downloadPath);
+        }
+        isDownloading = false;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+import java.net.URL;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello");
+        Downloader downloader = new Downloader();
+        try {
+            downloader.downLoad("https://www.journaldev.com/sitemap.xml", "/Users/karan.aulakh/Desktop/Karan/Downloader/downloaded_files");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
