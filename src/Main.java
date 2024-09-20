01caadaf6f31ac57afde7fd33f3c2ff9ff348751
Karan Aulakh <karan.aulakh@workday.com>
2024-09-20 13:12:54 -0600

    Added preset files for Recipes or Guitar guides

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
 import java.io.IOException;
-import java.util.concurrent.ExecutorService;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length != 1 || (!args[0].equals("recipes") && !args[0].equals("guitars"))) {
+            System.out.println("Please enter exactly only one of the following arguments: \"recipes\" or \"pictures\"");
+            return;
+        }
         Downloader downloader = new Downloader();
-        try {
-            downloader.downLoad("http://rarerecipes.net/veg_manchurian_recipe.pdf", "/Users/karan.aulakh/Desktop/Karan/Downloader/downloaded_files/test.pdf");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+
+        final List<Map.Entry<String, String>> downloads;
+        downloads = Util.getDownloads(args[0]);
+        for (Map.Entry<String, String> entry : downloads){
+            try {
+                downloader.downLoad(entry.getValue(), "/Users/karan.aulakh/Desktop/Karan/FileDownloader/downloaded_files/" + entry.getKey() + ".pdf");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
