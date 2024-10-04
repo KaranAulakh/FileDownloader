ff6a5c47cc2e1618a7ed0d63c1794032e183a75f
Karan Aulakh <karan.aulakh@workday.com>
2024-10-04 14:22:00 -0600

    Create command line interface

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,49 @@
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length != 1 || (!args[0].equals("recipes") && !args[0].equals("guitars"))) {
-            System.out.println("Please enter exactly only one of the following arguments: \"recipes\" or \"pictures\"");
+        Downloader downloader = new Downloader();
+        if (args.length < 2) {
+            menu(downloader);
+        }
+        else if (args.length != 1 || (!args[0].equals("recipes") && !args[0].equals("guitars"))) {
+            System.out.println("Please enter zero arguments for menu or exactly only one of the following arguments for test run: \"recipes\" or \"guitars\"");
             return;
         }
-        Downloader downloader = new Downloader();
+        else {
+            downloadSet(args[0], downloader);
+        }
 
-        final List<Map.Entry<String, String>> downloads;
-        downloads = Util.getDownloads(args[0]);
-        for (Map.Entry<String, String> entry : downloads){
+
+    }
+
+    public static void menu(Downloader downloader) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Welcome to File Downloader! Please enter the path of the file you wish to download" +
+                " or \"recipes\" or \"guitars\". You may also enter /quit to leave the program");
+
+        while (true) {
+            String option = userInput.nextLine();
+            if (Objects.equals(option, "/quit")){
+                return;
+            }
+            else if (Objects.equals(option, "guitars")) {
+                downloadSet("guitars", downloader);
+            }
+            else if (Objects.equals(option, "recipes")) {
+                downloadSet("recipes", downloader);
+            }
+            System.out.println("I'll get started on that, what else would you like to download?");
+        }
+    }
+
+    public static void downloadSet(String option, Downloader downloader){
+        final Map.Entry<String, String>[] downloads;
+        downloads = Util.getDownloads(option);
+        for (Map.Entry<String, String> entry : downloads) {
             try {
                 downloader.downLoad(entry.getValue(), "/Users/karan.aulakh/Desktop/Karan/FileDownloader/downloaded_files/" + entry.getKey() + ".pdf");
             } catch (IOException e) {
--- src/Util.java
+++ src/Util.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
 public class Util {
-    public static final List<Map.Entry<String, String>> recipes = Arrays.asList(
+    public static final Map.Entry<String, String>[] recipes = new Map.Entry[] {
             new AbstractMap.SimpleEntry<>("Veg Manchurian Recipe", "http://rarerecipes.net/veg_manchurian_recipe.pdf"),
             new AbstractMap.SimpleEntry<>("Veg Recipe Cookbook", "https://www.angelfire.com/ultra/indiandelights/vastrecipes.pdf")
-    );
+    };
 
-    public static final List<Map.Entry<String, String>> guitars = Arrays.asList(
+    public static final Map.Entry<String, String>[] guitars = new Map.Entry[]{
             new AbstractMap.SimpleEntry<>("Taylor Wood and Steel", "https://woodandsteel.taylorguitars.com/wp-content/uploads/2023/02/3675-WoodSteel-Product-Guide-2023.pdf"),
             new AbstractMap.SimpleEntry<>("Guitar all-in-one for Dummies", "http://160592857366.free.fr/joe/guitar%20tabs/184%20Full%20Album%20Guitar%20Tab%20Books%20.pdf/Guitar%20All-in-One%20for%20Dummies.pdf"),
             new AbstractMap.SimpleEntry<>("Yamaha: Choosing an Electric", "https://www.yamaha.com/US/houseofworship/downloadables/How-to-Choose-Guides/Yamaha-Guide-to-Choosing-an-Electric-Guitar.pdf"),
             new AbstractMap.SimpleEntry<>("Emily by Julian Lage", "https://mistletoemusicschool.com/blog/wp-content/uploads/2020/05/Emily-Julian-Lage.pdf")
-    );
+    };
 
-    public static List<Map.Entry<String, String>>getDownloads(String typeOfDownload){
+    public static Map.Entry<String, String>[] getDownloads(String typeOfDownload){
         if (typeOfDownload.equals("recipes")){
             return recipes;
         }
